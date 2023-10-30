27489a955ed822793b61b2e4d9a44f8a26abd94e
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 12:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const btn = document.getElementById("enterBtn");
 const para = document.getElementById("status");
 btn.addEventListener("click", fun);
 function fun(){
-	para.remove();
+	para.innerText = "";
 	const heading = document.createElement("h1");
 	heading.innerText = "Entered Metaverse";
 	heading.id = "status";
 	const parent = document.getElementsByTagName("body")[0];
-	parent.insertBefore(heading, btn);
+	para.appendChild(heading);
 }
